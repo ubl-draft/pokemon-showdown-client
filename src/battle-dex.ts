921eb17493e677615695a05b10ede9f80eac99b9
Karthik99999 <bandagondak0217@gmail.com>
2021-02-28 13:48:25 -0500

    allow use of custom avatars

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -217,7 +217,7 @@ const Dex = new class implements ModdedDex {
 		if (avatar.charAt(0) === '#') {
 			return Dex.resourcePrefix + 'sprites/trainers-custom/' + toID(avatar.substr(1)) + '.png';
 		}
-		if (avatar.includes('.') && window.Config?.server?.registered) {
+		if (avatar.includes('.')) {
 			// custom avatar served by the server
 			let protocol = (Config.server.port === 443) ? 'https' : 'http';
 			return protocol + '://' + Config.server.host + ':' + Config.server.port +
